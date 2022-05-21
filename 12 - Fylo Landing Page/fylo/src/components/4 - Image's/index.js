0d21323c1ddc982089b4995e4fb183d89af679da
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-05-21 10:17:50 -0300

    component img styled and props on function created

--- 12 - Fylo Landing Page/fylo/src/components/4 - Image's/index.js
+++ 12 - Fylo Landing Page/fylo/src/components/4 - Image's/index.js
@@ -1,5 +1,27 @@
-function Img () {
+function Img (props) {
+    if(props.fyloLogo){
+        return <fyloLogo src={props.src} alt={props.alt}/>
+    }
+    
+    else if (props.headerImg){
+        return <headerImg src={props.src} alt={props.alt}/>
+    }
 
+    else if (props.testemonialImg){
+        return <testemonialImg src={props.src} alt={props.alt}/>
+    }
+
+    else if (props.signKyleImg){
+        return <signKyleImg src={props.src} alt={props.alt}/>
+    }
+
+    else if(props.iconPhone){
+        return <iconPhone src={props.src} alt={props.alt}/>
+    }
+
+    else if (props.iconEmail){
+        return <iconEmail src={props.src} alt={props.alt}/>
+    }
 }
 
 export default Img;
--- 12 - Fylo Landing Page/fylo/src/components/4 - Image's/styles.js
+++ 12 - Fylo Landing Page/fylo/src/components/4 - Image's/styles.js
@@ -1,5 +1,25 @@
 import styled from "styled-components"
 
-export const Img = styled.img`
+export const fyloLogo = styled.img`
+
+`
+
+export const headerImg = styled.img`
+
+`
+
+export const testemonialImg = styled.img`
+
+`
+
+export const signKyleImg = styled.img`
+
+`
+
+export const iconPhone = styled.img`
+
+`
+
+export const iconEmail = styled.img`
 
 `
