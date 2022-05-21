536ac52aa4b12fe23df3aec312a2f116eb7f0917
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-05-21 10:30:08 -0300

    component subtitle styled and props on function created

--- 12 - Fylo Landing Page/fylo/src/components/2 - Subtitle/index.js
+++ 12 - Fylo Landing Page/fylo/src/components/2 - Subtitle/index.js
@@ -1,5 +1,15 @@
 function Subtitle (props) {
-    
+    if(props.subtitleHeader){
+        return <subtitleHeader content={props.content}>{props.content}</subtitleHeader>
+    }
+
+    else if (props.subtitleTestemonial){
+        return <subtitleTestemonial content={props.content}>{props.content}</subtitleTestemonial>
+    }
+
+    else if (props.subtitleSignUp){
+        return <subtitleSignUp content={props.content}>{props.content}</subtitleSignUp>
+    }
 }
 
 export default Subtitle;
--- 12 - Fylo Landing Page/fylo/src/components/2 - Subtitle/styles.js
+++ 12 - Fylo Landing Page/fylo/src/components/2 - Subtitle/styles.js
@@ -1,5 +1,13 @@
 import styled from "styled-components"
 
-export const Subtitle = styled.p`
+export const subtitleHeader = styled.p`
+
+`
+
+export const subtitleSignUp = styled.p`
+
+`
+
+export const subtitleTestemonial = styled.p`
 
 `
